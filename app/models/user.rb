4f7145c5d266df25f150b0ce71262acbfa87fceb
kuz-en <bio19@ya.ru>
2021-04-14 22:13:08 +0300

    Edit layout for Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,18 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # Юзер может создавать много событий
+
   has_many :events
 
-  # У юреза должно быть имя не длиннее 35 букв
-  validates :name, presence: true, length: {maximum: 35}
+  validates :name, presence: true, length: { maximum: 35 }
 
-  # У юзера должен быть уникальный email по заданному шаблону не длиннее 255 символов
-  validates :email, presence: true, length: {maximum: 255}
-  validates :email, uniqueness: true
-  validates :email, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
+  validates :email, presence: true, uniqueness: true
+
+  before_validation :set_name, on: :create
+
+  private
+
+  def set_name
+    self.name = 'Незнакомец' if self.name.blank?
+  end
 end
