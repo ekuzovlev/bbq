74cb57bb305cadfd57a1d7d44a9257618e560768
kuz-en <bio19@ya.ru>
2021-04-20 12:01:35 +0300

    Subscription model updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :events
-  has_many :comments
-  has_many :subscriptions
+  has_many :events, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :subscriptions, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 35 }
 
@@ -12,9 +12,16 @@ class User < ApplicationRecord
 
   before_validation :set_name, on: :create
 
+  after_commit :link_subscriptions, on: :create
+
   private
 
   def set_name
     self.name = 'Незнакомец' if self.name.blank?
+  end
+
+  def link_subscriptions
+    Subscription.where(user_id: nil, user_email: self.email)
+                .update_all(user_id: self.id)
   end
 end
