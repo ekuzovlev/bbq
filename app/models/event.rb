aea16e6ab6e10ccd375100bdb459f710b75e8a51
kuz-en <bio19@ya.ru>
2021-04-21 12:10:08 +0300

    Model Photo created

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,15 +1,17 @@
 class Event < ApplicationRecord
-  # Событие принадлежит юзеру
   belongs_to :user
 
   has_many :comments
   has_many :subscriptions
   has_many :subscribers, through: :subscriptions, source: :user
+  has_many :photos
 
-  # Заголовок должен быть, и не может быть длиннее 255 букв
-  validates :title, presence: true, length: {maximum: 255}
+  validates :title, presence: true, length: { maximum: 255 }
 
-  # Также у события должны быть заполнены место и время проведения
   validates :address, presence: true
   validates :datetime, presence: true
+
+  def visitors
+    (subscribers + [user]).uniq
+  end
 end
