4d3e59564b952cee38e516f9635ed19f64300461
kuz-en <bio19@ya.ru>
2021-04-26 15:58:56 +0300

    Config env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,16 +53,16 @@ Rails.application.configure do
 
   # Базовый URL сайта для правильных ссылок в письмах
   # ПРОПИСЫВАЙТЕ свой!
-  config.action_mailer.default_url_options = {host: 'bbq-show.herokuapp.com'}
+  # config.action_mailer.default_url_options = {host: 'bbq-show.herokuapp.com'}
 
   # Ошибки рассылки юзеру не показываем
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Делать рассылку писем (при false приложение только имитирует отправку)
   config.action_mailer.perform_deliveries = true
 
   # Устанавливаем протокол, по которому отправлять (SMTP)
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :smtp
 
   # А это для SendGrid
   # ActionMailer::Base.smtp_settings = {
