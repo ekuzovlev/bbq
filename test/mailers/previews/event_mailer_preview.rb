5832fd871572eb1bba302d2b54cbd09fbdae6e51
kuz-en <bio19@ya.ru>
2021-05-06 19:26:46 +0300

    Add link to email with comment

--- test/mailers/previews/event_mailer_preview.rb
+++ test/mailers/previews/event_mailer_preview.rb
@@ -1,4 +1,8 @@
 class EventMailerPreview < ActionMailer::Preview
+  def comment
+    EventMailer.comment(Event.last, Event.find(3).comments.last, Event.last.user.email)
+  end
+
   def photo
     EventMailer.photo(Event.last, Photo.last, Event.last.user.email)
   end
