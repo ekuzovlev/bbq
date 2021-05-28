fa86310b57fc282eee7123a9bcba169aac20e935
kuz-en <bio19@ya.ru>
2021-05-28 23:43:15 +0300

    Change user model for FB image

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
   def self.find_for_facebook_oauth(access_token)
     email = access_token.info.email
     user = where(email: email).first
-    fb_avatar = access_token.info.image
+    fb_avatar = URI.parse(access_token.info.image)
 
     user&.update(remote_avatar_url: fb_avatar) unless user&.avatar.present?
 
