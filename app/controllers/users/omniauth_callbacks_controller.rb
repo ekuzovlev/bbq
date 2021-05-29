9e176b071d04774fc9d8782391ffcf5aedbbbee7
kuz-en <bio19@ya.ru>
2021-05-29 12:51:08 +0300

    Fix omniauth provider methods

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,11 +2,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_action :verify_authenticity_token, only: :facebook
 
   def facebook
-    oauth_user(facebook)
+    oauth_user('facebook')
   end
 
   def vkontakte
-    oauth_user(vkontakte)
+    oauth_user('vkontakte')
   end
 
   private
@@ -15,12 +15,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_for_oauth(request.env['omniauth.auth'])
 
     if @user.persisted?
-      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.to_s)
+      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider)
       sign_in_and_redirect @user, event: :authentication
     else
       flash[:error] = I18n.t(
         'devise.omniauth_callbacks.failure',
-        kind: provider.to_s,
+        kind: provider,
         reason: 'authentication error'
       )
 
